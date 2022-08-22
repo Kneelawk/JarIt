4176bfa2e31c22e0ff85edbdb002e3da588a432a
Kneelawk <kneelawk@gmail.com>
2022-08-21 19:57:48 -0700

    Fix BE copying

---
+++ changelogs/changelog-v0.1.3+1.19.2.md
@@ -0,0 +1,9 @@
+# Jar It! v0.1.3+1.19.2
+
+Jar It! version 0.1.3 for Minecraft 1.19.2
+
+Changes:
+
+* Jars now copy block-entity NBT properly.
+* Made it so that shrinking a jar places the jar block at the bottom and expanding a jar assumes the jar being expanded
+  is at the bottom.
--- src/main/kotlin/com/kneelawk/jarit/dimension/JarPlacement.kt
+++ src/main/kotlin/com/kneelawk/jarit/dimension/JarPlacement.kt
@@ -3,6 +3,9 @@ package com.kneelawk.jarit.dimension
 import com.kneelawk.jarit.block.Blocks
 import com.kneelawk.jarit.blockentity.JarBlockEntity
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockBox
 import net.minecraft.util.math.BlockPos
@@ -14,9 +17,9 @@ import net.minecraft.block.Blocks as MCBlocks
 object JarPlacement {
     private const val REGION_SIZE = 32 * 16
 
-    private val GLASS_STATE by lazy { Blocks.JAR_INSIDE_GLASS.defaultState }
-    private val CORK_STATE by lazy { Blocks.JAR_INSIDE_CORK.defaultState }
-    private val OUTSIZE_GLASS_STATE by lazy { Blocks.JAR_GLASS.defaultState }
+    private val INSIDE_GLASS_STATE by lazy { Blocks.JAR_INSIDE_GLASS.defaultState }
+    private val INSIDE_CORK_STATE by lazy { Blocks.JAR_INSIDE_CORK.defaultState }
+    private val OUTSIDE_GLASS_STATE by lazy { Blocks.JAR_GLASS.defaultState }
 
     fun getJarStart(id: Long, maxSize: Int): BlockPos {
         val jarSize = maxSize + 2
@@ -55,111 +58,6 @@ object JarPlacement {
         return BlockPos(x + regionX * REGION_SIZE, y, z + regionZ * REGION_SIZE)
     }
 
-    fun placeJar(world: World, id: Long, info: JarInfo, maxJarSize: Int) {
-        val start = getJarStart(id, maxJarSize)
-        val mutable = BlockPos.Mutable()
-
-        // floor
-        for (z in 0 until info.size + 2) {
-            for (x in 0 until info.size + 2) {
-                mutable.set(start, x, 0, z)
-                world.setBlockState(mutable, GLASS_STATE)
-            }
-        }
-
-        // -x wall
-        for (y in 1 until info.size + 1) {
-            for (z in 0 until info.size + 2) {
-                mutable.set(start, 0, y, z)
-                world.setBlockState(mutable, GLASS_STATE)
-            }
-        }
-
-        // -z wall
-        for (y in 1 until info.size + 1) {
-            for (x in 1 until info.size + 1) {
-                mutable.set(start, x, y, 0)
-                world.setBlockState(mutable, GLASS_STATE)
-            }
-        }
-
-        // +x wall
-        for (y in 1 until info.size + 1) {
-            for (z in 0 until info.size + 2) {
-                mutable.set(start, info.size + 1, y, z)
-                world.setBlockState(mutable, GLASS_STATE)
-            }
-        }
-
-        // +z wall
-        for (y in 1 until info.size + 1) {
-            for (x in 1 until info.size + 1) {
-                mutable.set(start, x, y, info.size + 1)
-                world.setBlockState(mutable, GLASS_STATE)
-            }
-        }
-
-        // cork
-        for (z in 0 until info.size + 2) {
-            for (x in 0 until info.size + 2) {
-                mutable.set(start, x, info.size + 1, z)
-                world.setBlockState(mutable, CORK_STATE)
-            }
-        }
-    }
-
-    fun placeOutsideJar(world: World, start: BlockPos, jarSize: Int) {
-        val mutable = BlockPos.Mutable()
-
-        // floor
-        for (z in 0 until jarSize + 2) {
-            for (x in 0 until jarSize + 2) {
-                mutable.set(start, x, 0, z)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-
-        // -x wall
-        for (y in 1 until jarSize + 1) {
-            for (z in 0 until jarSize + 2) {
-                mutable.set(start, 0, y, z)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-
-        // -z wall
-        for (y in 1 until jarSize + 1) {
-            for (x in 1 until jarSize + 1) {
-                mutable.set(start, x, y, 0)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-
-        // +x wall
-        for (y in 1 until jarSize + 1) {
-            for (z in 0 until jarSize + 2) {
-                mutable.set(start, jarSize + 1, y, z)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-
-        // +z wall
-        for (y in 1 until jarSize + 1) {
-            for (x in 1 until jarSize + 1) {
-                mutable.set(start, x, y, jarSize + 1)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-
-        // top
-        for (z in 0 until jarSize + 2) {
-            for (x in 0 until jarSize + 2) {
-                mutable.set(start, x, jarSize + 1, z)
-                world.setBlockState(mutable, OUTSIZE_GLASS_STATE)
-            }
-        }
-    }
-
     fun capture(world: ServerWorld, start: BlockPos, fullJarSize: Int) {
         val server = world.server
 
@@ -180,42 +78,13 @@ object JarPlacement {
 
         println("Placing new jar at: $toStart")
 
-        val fromMut = fromStart.mutableCopy()
-        val toMut = toStart.mutableCopy()
-        for (y in (0 until jarSize).reversed()) {
-            for (z in 0 until jarSize) {
-                for (x in 0 until jarSize) {
-                    fromMut.set(fromStart, x, y, z)
-                    toMut.set(toStart, x, y, z)
-                    jarDim.setBlockState(toMut, world.getBlockState(fromMut), Block.NOTIFY_LISTENERS)
-                    world.setBlockState(fromMut, MCBlocks.AIR.defaultState, Block.NOTIFY_LISTENERS)
-                }
-            }
-        }
+        copyContents(fromStart, toStart, world, jarDim, jarSize)
 
-        val insideArea = BlockBox(
-            fromStart.x, fromStart.y, fromStart.z, fromStart.x + jarSize, fromStart.y + jarSize, fromStart.z + jarSize
-        )
+        movePlayers(fromStart, toStart, world, jarDim, jarSize)
 
-        val offset = Vec3d.of(toStart.subtract(fromStart))
+        clearArea(world, start, fullJarSize)
 
-        val players = world.getPlayers { insideArea.contains(it.blockPos) }
-        players.forEach {
-            val oldPos = it.pos
-            val newPos = oldPos.add(offset)
-            it.teleport(jarDim, newPos.x, newPos.y, newPos.z, it.headYaw, it.pitch)
-        }
-
-        for (y in (0 until fullJarSize).reversed()) {
-            for (z in 0 until fullJarSize) {
-                for (x in 0 until fullJarSize) {
-                    fromMut.set(start, x, y, z)
-                    world.setBlockState(fromMut, MCBlocks.AIR.defaultState, Block.NOTIFY_LISTENERS)
-                }
-            }
-        }
-
-        val putJar = BlockPos(start.x + fullJarSize / 2, start.y + (fullJarSize - 1), start.z + fullJarSize / 2)
+        val putJar = BlockPos(start.x + fullJarSize / 2, start.y, start.z + fullJarSize / 2)
 
         world.setBlockState(putJar, Blocks.JAR.defaultState)
         (world.getBlockEntity(putJar) as JarBlockEntity).updateJarId(jarId)
@@ -237,47 +106,137 @@ object JarPlacement {
         val fullJarSize = jarSize + 2
 
         val dimStart = getJarStart(jarId, jarDimInfo.maxJarSize)
-        val jarStart = BlockPos(jar.x - fullJarSize / 2, jar.y - (fullJarSize - 1), jar.z - fullJarSize / 2)
+        val jarStart = BlockPos(jar.x - fullJarSize / 2, jar.y, jar.z - fullJarSize / 2)
 
         val fromStart = dimStart.add(BlockPos(1, 1, 1))
         val toStart = jarStart.add(BlockPos(1, 1, 1))
 
         placeOutsideJar(world, jarStart, jarSize)
 
-        val fromMut = fromStart.mutableCopy()
-        val toMut = toStart.mutableCopy()
+        copyContents(fromStart, toStart, jarDim, world, jarSize)
+
+        movePlayers(fromStart, toStart, jarDim, world, jarSize)
+
+        clearArea(jarDim, dimStart, jarDimInfo.maxJarSize + 2)
+
+        jarDimInfo.removeJar(jarId)
+    }
+
+    private fun placeJar(world: World, id: Long, info: JarInfo, maxJarSize: Int) {
+        placeJar(world, getJarStart(id, maxJarSize), info.size, INSIDE_GLASS_STATE, INSIDE_CORK_STATE)
+    }
+
+    private fun placeOutsideJar(world: World, start: BlockPos, jarSize: Int) {
+        placeJar(world, start, jarSize, OUTSIDE_GLASS_STATE, OUTSIDE_GLASS_STATE)
+    }
+
+    private fun placeJar(world: World, start: BlockPos, jarSize: Int, glassState: BlockState, corkState: BlockState) {
+        val mutable = BlockPos.Mutable()
+
+        // floor
+        for (z in 0 until jarSize + 2) {
+            for (x in 0 until jarSize + 2) {
+                mutable.set(start, x, 0, z)
+                world.setBlockState(mutable, glassState)
+            }
+        }
+
+        // -x wall
+        for (y in 1 until jarSize + 1) {
+            for (z in 0 until jarSize + 2) {
+                mutable.set(start, 0, y, z)
+                world.setBlockState(mutable, glassState)
+            }
+        }
+
+        // -z wall
+        for (y in 1 until jarSize + 1) {
+            for (x in 1 until jarSize + 1) {
+                mutable.set(start, x, y, 0)
+                world.setBlockState(mutable, glassState)
+            }
+        }
+
+        // +x wall
+        for (y in 1 until jarSize + 1) {
+            for (z in 0 until jarSize + 2) {
+                mutable.set(start, jarSize + 1, y, z)
+                world.setBlockState(mutable, glassState)
+            }
+        }
+
+        // +z wall
+        for (y in 1 until jarSize + 1) {
+            for (x in 1 until jarSize + 1) {
+                mutable.set(start, x, y, jarSize + 1)
+                world.setBlockState(mutable, glassState)
+            }
+        }
+
+        // top
+        for (z in 0 until jarSize + 2) {
+            for (x in 0 until jarSize + 2) {
+                mutable.set(start, x, jarSize + 1, z)
+                world.setBlockState(mutable, corkState)
+            }
+        }
+    }
+
+    private fun copyContents(
+        fromStart: BlockPos, toStart: BlockPos, fromWorld: ServerWorld, toWorld: ServerWorld, jarSize: Int
+    ) {
+        val fromMut = BlockPos.Mutable()
+        val toMut = BlockPos.Mutable()
         for (y in (0 until jarSize).reversed()) {
             for (z in 0 until jarSize) {
                 for (x in 0 until jarSize) {
                     fromMut.set(fromStart, x, y, z)
                     toMut.set(toStart, x, y, z)
-                    world.setBlockState(toMut, jarDim.getBlockState(fromMut), Block.NOTIFY_LISTENERS)
+                    toWorld.setBlockState(toMut, fromWorld.getBlockState(fromMut), Block.NOTIFY_LISTENERS)
+
+                    fromWorld.getBlockEntity(fromMut)?.let { oldBE ->
+                        val tag = oldBE.toNbt()
+                        modifyNBT(tag, toMut)
+                        toWorld.getBlockEntity(toMut)?.readNbt(tag)
+                    }
                 }
             }
         }
+    }
 
+    private fun modifyNBT(tag: NbtCompound, toMut: BlockPos) {
+        if (tag.contains("x", NbtElement.NUMBER_TYPE.toInt())) tag.putInt("x", toMut.x)
+        if (tag.contains("y", NbtElement.NUMBER_TYPE.toInt())) tag.putInt("y", toMut.y)
+        if (tag.contains("z", NbtElement.NUMBER_TYPE.toInt())) tag.putInt("z", toMut.z)
+    }
+
+    private fun movePlayers(
+        fromStart: BlockPos, toStart: BlockPos, fromWorld: ServerWorld, toWorld: ServerWorld, jarSize: Int
+    ) {
         val insideArea = BlockBox(
             fromStart.x, fromStart.y, fromStart.z, fromStart.x + jarSize, fromStart.y + jarSize, fromStart.z + jarSize
         )
 
         val offset = Vec3d.of(toStart.subtract(fromStart))
 
-        val players = jarDim.getPlayers { insideArea.contains(it.blockPos) }
+        val players = fromWorld.getPlayers { insideArea.contains(it.blockPos) }
         players.forEach {
             val oldPos = it.pos
             val newPos = oldPos.add(offset)
-            it.teleport(world, newPos.x, newPos.y, newPos.z, it.headYaw, it.pitch)
+            it.teleport(toWorld, newPos.x, newPos.y, newPos.z, it.headYaw, it.pitch)
         }
+    }
 
-        for (y in (0 until jarDimInfo.maxJarSize + 2).reversed()) {
-            for (z in 0 until jarDimInfo.maxJarSize + 2) {
-                for (x in 0 until jarDimInfo.maxJarSize + 2) {
-                    fromMut.set(dimStart, x, y, z)
-                    jarDim.setBlockState(fromMut, MCBlocks.AIR.defaultState, Block.NOTIFY_LISTENERS)
+    private fun clearArea(world: ServerWorld, start: BlockPos, fullJarSize: Int) {
+        val mut = BlockPos.Mutable()
+        for (y in (0 until fullJarSize).reversed()) {
+            for (z in 0 until fullJarSize) {
+                for (x in 0 until fullJarSize) {
+                    mut.set(start, x, y, z)
+                    world.removeBlockEntity(mut)
+                    world.setBlockState(mut, MCBlocks.AIR.defaultState, Block.NOTIFY_LISTENERS)
                 }
             }
         }
-
-        jarDimInfo.removeJar(jarId)
     }
 }
